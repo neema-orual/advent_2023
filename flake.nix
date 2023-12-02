1ac645dd1cd4e3ed56d9f58cd84cfe207b8969bb
Neema <neema.brown@proton.me>
2023-12-02 17:23:13 -0500

    switch advent to rust for the moment

--- flake.nix
+++ flake.nix
@@ -1,80 +1,40 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
-    systems.url = "github:nix-systems/default";
-    devenv.url = "github:cachix/devenv";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    esp32 = {
+      url = "github:Radiant-Industries/esp32";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  nixConfig = {
-    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
-    extra-substituters = "https://devenv.cachix.org";
-  };
-
-  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
+  outputs =
+    { self
+    , nixpkgs
+    , esp32
+    ,
+    }:
     let
-      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+      idf-rust = esp32.packages.x86_64-linux.esp32;
     in
     {
-      packages = forEachSystem (system: {
-        devenv-up = self.devShells.${system}.default.config.procfileScript;
-      });
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          idf-rust
+          pkg-config
+          libuv
+          systemd
+          openssl
+          openssl.dev
+        ];
 
-      devShells = forEachSystem
-        (system:
-          let
-            pkgs = nixpkgs.legacyPackages.${system};
-          in
-          {
-            default = devenv.lib.mkShell {
-              inherit inputs pkgs;
-              modules = [
-                {
-                  languages.c.enable = true;
-                  env.name = "advent";
-                  # https://devenv.sh/reference/options/
-                  packages = [
-                    pkgs.cc65
-                    pkgs.c64-debugger
-                    pkgs.minipro
-                  ];
-
-                  enterShell = ''
-                    cc65 --version
-                  '';
-
-                  scripts.build.exec = ''
-                    ca65 -vvv --cpu 6502 -l build/listing.txt -o build/abn6507rom.o abn6507rom.s
-                    ld65 -o build/abn6507rom.bin -C memmap.cfg "./build/abn6507rom.o" #"./build/userland.o" #"./build/crom.o"
-                  '';
-
-                  scripts.upload.exec = ''
-                    ca65 -vvv --cpu 6502 -l build/listing.txt -o build/abn6507rom.o abn6507rom.s
-                    ld65 -o build/abn6507rom.bin -C memmap.cfg "./build/abn6507rom.o" #"./build/userland.o" #"./build/crom.o"
-
-                    serial="/dev/ttyUSB0" #linux
-                    eval serial=$serial
-                    cat -v < $serial & #Keep serial alive
-                    pid=$! #Save for later
-                    stty -F $serial $baudrate
-                    echo -n $'\x01' > $serial #Send SOH to get 65uino ready to receive
-                    sleep 0.1 #Wait for timeout
-                    cat build/userland.bin > $serial
-                    kill $pid #Terminate serial
-                    wait $pid 2>/dev/null #silence!
-                  '';
-
-                  scripts.flash.exec = ''
-                    ca65 -vvv --cpu 6502 -l build/listing.txt -o build/abn6507rom.o abn6507rom.s
-                    ld65 -o build/abn6507rom.bin -C memmap.cfg "./build/abn6507rom.o" #"./build/crom.o" #"./build/userland.o"
-                    minipro -s -p "SST39SF010A" -w build/abn6507rom.bin
-                  '';
-
-                  scripts.push.exec = ''
-
-                  '';
-                }
-              ];
-            };
-          });
+        shellHook = ''
+          export PATH="${idf-rust}/.rustup/toolchains/esp/bin:$PATH"
+          export PATH="$HOME/.cargo/bin:$PATH"
+          export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
+          export RUSTUP_HOME="${idf-rust}/.rustup"
+          source ./.export-esp.sh
+        '';
+      };
     };
 }
